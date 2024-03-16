e7c3ce5369a88139c5f3acf0a8618f79417d2d55
nikolastefanov <nikoay_stefanov1965@abv.bg>
2024-03-16 21:08:15 +0200

    SignlRChatRepoBlazor

---
+++ .vs/SignalRChatBlazor/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace SignalRChatBlazor.Hubs
+{
+    using Microsoft.AspNetCore.SignalR;
+
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+    }
+}
